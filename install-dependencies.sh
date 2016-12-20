f4eeb21b73b6e11de8b9a2075c38c7a6ab0bc5a3
Michael Walter <michael.walter@swel.com>
2016-12-20 15:39:46 +0100

    use develop branch of fplll for develop branch of fpylll

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-git clone https://github.com/dstehle/fplll
+git clone -b develop https://github.com/dstehle/fplll
 
 cd fplll
+#~ git checkout 8a45a4b828830a596bd4e87ca0798c52f64ec65c
 ./autogen.sh
 if test "$1" != ""; then
     ./configure --prefix="$1"
